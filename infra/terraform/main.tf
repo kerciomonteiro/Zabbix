ce6ee08a1efd45caab6a3b3f88c83159a79ec21a
kerciomonteiro <kercio.monteiro@forescout.com>
2025-07-17 14:11:30 -0500

    Importing TFs

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,6 +1,36 @@
 # Terraform configuration for Zabbix AKS Infrastructure
 # This replaces the Bicep template with Terraform for better multi-cloud support
 
+import {
+  to = azurerm_user_assigned_identity.aks
+  id = "/subscriptions/d9b2a1cf-f99b-4f9e-a6cf-c79a078406bf/resourceGroups/Devops-Test/providers/Microsoft.ManagedIdentity/userAssignedIdentities/id-devops-eastus"
+}
+
+import {
+  to = azurerm_log_analytics_workspace.main[0]
+  id = "/subscriptions/d9b2a1cf-f99b-4f9e-a6cf-c79a078406bf/resourceGroups/Devops-Test/providers/Microsoft.OperationalInsights/workspaces/law-devops-eastus"
+}
+
+import {
+  to = azurerm_network_security_group.aks
+  id = "/subscriptions/d9b2a1cf-f99b-4f9e-a6cf-c79a078406bf/resourceGroups/Devops-Test/providers/Microsoft.Network/networkSecurityGroups/nsg-aks-devops-eastus"
+}
+
+import {
+  to = azurerm_network_security_group.appgw
+  id = "/subscriptions/d9b2a1cf-f99b-4f9e-a6cf-c79a078406bf/resourceGroups/Devops-Test/providers/Microsoft.Network/networkSecurityGroups/nsg-appgw-devops-eastus"
+}
+
+import {
+  to = azurerm_virtual_network.main
+  id = "/subscriptions/d9b2a1cf-f99b-4f9e-a6cf-c79a078406bf/resourceGroups/Devops-Test/providers/Microsoft.Network/virtualNetworks/vnet-devops-eastus"
+}
+
+import {
+  to = azurerm_public_ip.appgw
+  id = "/subscriptions/d9b2a1cf-f99b-4f9e-a6cf-c79a078406bf/resourceGroups/Devops-Test/providers/Microsoft.Network/publicIPAddresses/pip-appgw-devops-eastus"
+}
+
 terraform {
   required_version = ">= 1.0"
   required_providers {
